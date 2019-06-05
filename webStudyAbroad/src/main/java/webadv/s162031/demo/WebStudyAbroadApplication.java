3ce036bba08aec44d271e24984952436160a02de
SXXXXXX <16203122@stu.nchu.edu.cn>
2019-06-05 14:34:26 +0800

    提交了项目模板

---
+++ webStudyAbroad/src/main/java/webadv/s162031/demo/WebStudyAbroadApplication.java
@@ -0,0 +1,13 @@
+package webadv.s162031.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStudyAbroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStudyAbroadApplication.class, args);
+	}
+
+}

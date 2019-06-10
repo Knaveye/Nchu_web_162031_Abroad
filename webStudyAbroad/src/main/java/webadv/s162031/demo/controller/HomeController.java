546e969c3129cd0aab7b4426d73af67019df2b8a
s16203123 <1005633750@qq.com>
2019-06-10 17:13:24 +0800

    first commit

--- webStudyAbroad/src/main/java/webadv/s162031/demo/controller/HomeController.java
+++ webStudyAbroad/src/main/java/webadv/s162031/demo/controller/HomeController.java
@@ -10,5 +10,58 @@ public class HomeController {
 	public String index() {
 		return "login";
 	}
-
+	 @GetMapping("/news")
+	    public String tonews(){
+	    	return "news";
+	    }
+	    
+	    @GetMapping("/index")
+	    public String toindex(){
+	    	return "index";
+	    }
+	    @GetMapping("/newslist")
+	    public String tonewslist(){
+	    	return "newslist";
+	    }
+	    
+	    @GetMapping("/announcement")
+	    public String toannouncement(){
+	    	return "announcement";
+	    }
+	    @GetMapping("/annDeatils")
+	    public String toannDeatils(){
+	    	return "annDeatils";
+	    }
+	    @GetMapping("/declarationGuide")
+	    public String todeclarationGuide(){
+	    	return "declarationGuide";
+	    }
+	    @GetMapping("/admission")
+	    public String toadmission(){
+	    	return "admission";
+	    }
+	    @GetMapping("/foreginManage")
+	    public String toforeginManage(){
+	    	return "foreginManage";
+	    }
+	    @GetMapping("/cooperative")
+	    public String tocooperative(){//合作机构
+	    	return "cooperative";
+	    }
+	    @GetMapping("/manageRule")
+	    public String tomanageRule(){//管理规定
+	    	return "manageRule";
+	    }
+	    @GetMapping("/contact")
+	    public String tocontact(){//联系方式
+	    	return "contact";
+	    }
+	    @GetMapping("/institutionSet")
+	    public String toinstitutionSet(){//机构设置
+	    	return "institutionSet";
+	    }
+	    @GetMapping("/introduction")
+	    public String tointroduction(){//机构介绍
+	    	return "introduction";
+	    }
 }
